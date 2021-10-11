89963964bafa34d7cd26c91c4384a374d6479365
KangHongJun <40516203+KangHongJun@users.noreply.github.com>
2021-10-11 20:10:03 +0900

    11

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ from skimage.transform import resize
 import pyautogui
 
 pyautogui.screenshot('test.png')
+#1.  pyautogui를 이용하여 프로그램을 시작하고 클릭을 하면 그 위치를 기반으로 글자 인식
+#2. 알맞은 크기의 박스를 생성하여 배치하면 그곳에 있는(세부설정으로 닉네임 나오는 구간을 설정)텍스트만 인식
+
+
 
 
 pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
-config = ('-l kor+eng --oem 3 --psm 6')
+config = ('-l kor+eng --oem 3 --psm 11')
 
 
 img=cv2.imread("test.PNG")
-#img=resize(img,(1000,1000))
-
-#print(pytesseract.image_to_string("test.png",config=config))
+print(pytesseract.image_to_string("test.png",config=config))
 
 h, w, c = img.shape
 boxes = pytesseract.image_to_boxes(img)
